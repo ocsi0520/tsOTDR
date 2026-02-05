5e552666dee21812e2926b6bd48ab724332c523a
Faragó Tamás <ocsi0520@gmail.com>
2026-02-05 17:17:32 +0100

    add SupParams representation

---
+++ src/otdr/representation/SupParams.ts
@@ -0,0 +1,13 @@
+export type SupParams = {
+  supplierName: string;
+  Otdr: {
+    name: string;
+    serialNumber: string;
+  }
+  module: {
+    name: string;
+    serialNumber: string;
+  }
+  softwareVersion: number;
+  other: string;
+};
