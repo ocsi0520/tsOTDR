b2dfe4508f2894699c5469242925abbdafb7ae52
Faragó Tamás <ocsi0520@gmail.com>
2026-02-05 17:10:26 +0100

    fix GenParams

--- src/otdr/representation/GenParams.ts
+++ src/otdr/representation/GenParams.ts
@@ -1,31 +1,40 @@
 // REF: http://www.ciscopress.com/articles/article.asp?p=170740&seqNum=7
-type FiberType = "G.651 (50um core multimode)" | "G.652 (standard SMF)" | "G.653 (dispersion-shifted fiber)" | "G.654 (1550nm loss-minimzed fiber)" | "G.655 (nonzero dispersion-shifted fiber)" | "unknown"
+type FiberType =
+  | "G.651 (50um core multimode)"
+  | "G.652 (standard SMF)"
+  | "G.653 (dispersion-shifted fiber)"
+  | "G.654 (1550nm loss-minimzed fiber)"
+  | "G.655 (nonzero dispersion-shifted fiber)"
+  | "unknown";
 
-type BuildCondition = 
-| {raw: 'BC', normalized: 'as-built'}
-| {raw: 'CC', normalized: 'as-current'}
-| {raw: 'RC', normalized: 'as-repaired'}
-| {raw: 'OT', normalized: 'other'}
-| {raw: string, normalized: 'unknown'}
+type BuildCondition =
+  | { raw: "BC"; normalized: "as-built" }
+  | { raw: "CC"; normalized: "as-current" }
+  | { raw: "RC"; normalized: "as-repaired" }
+  | { raw: "OT"; normalized: "other" }
+  | { raw: string; normalized: "unknown" };
 
 export type GenParams = {
-    name: 'GenParams'
-    lang: string; // 2 chars
-    cableId: string; // assumption: read until '\0'
-    fiberId: string;
-    fiberType?: { // omitted in V1
-        raw: number // 2 bytes number
-        normalized: FiberType;
-    }
-    waveLengthInNm: number; // 2 bytes - nanometer
-    location: {
-        A: string;
-        B: string;
-    }
-    cableCodeOrFiberType: string;
-    buildCondition: BuildCondition;
-    userOffset: number; // signed 4 bytes
-    userOffsetDistance: number; // signed 4 bytes
-    operator: string;
-
-}
+  name: "GenParams";
+  lang: string; // 2 chars
+  cableId: string; // assumption: read until '\0'
+  fiberId: string;
+  fiberType?: {
+    // omitted in V1
+    raw: number; // 2 bytes unsigned number
+    normalized: FiberType;
+  };
+  waveLengthInNm: number; // 2 bytes unsigned - nanometer
+  location: {
+    A: string;
+    B: string;
+  };
+  cableCodeOrFiberType: string; // vary from vendor to vendor
+  buildCondition: BuildCondition; // 2 byte characters w/ out terminating '\0'
+  // don't know the units of the following 2
+  userOffset: number; // signed 4 bytes
+  userOffsetDistance?: number; // signed 4 bytes (only in version 2)
+  // string might contain \n or \r
+  operator: string;
+  comments: string;
+};
