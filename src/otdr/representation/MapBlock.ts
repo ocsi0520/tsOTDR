0eec383d1aee1226a0117333e686190c8f861107
Faragó Tamás <ocsi0520@gmail.com>
2026-02-05 17:01:42 +0100

    add MapBlock representation

---
+++ src/otdr/representation/MapBlock.ts
@@ -0,0 +1,27 @@
+export type RequiredBlockName = "GenParams" | "SupParams" | "FxdParams";
+// keyevent is required if no dataps is present and vica-versa
+export type OptionalBlockName = "KeyEvents" | "DataPts" | "LnkParams" | "Cksum";
+export type KnownBlockName = RequiredBlockName | OptionalBlockName;
+
+export type BlockDescriptor = {
+  name: KnownBlockName | string; // terminated by '\0'
+  version: number; // 2 bytes
+  // if the version is 1.2, then the original number from SOR is 120
+  normalizedVersion: number;
+  size: number; // 4 bytes
+
+  positionInBinaryFile: number;
+  index: number;
+};
+
+export type MapBlock = {
+  format: 1 | 2;
+  version: number;
+  // if the version is 1.2, then the original number from SOR is 120
+  normalizedVersion: number;
+  size: number; // nbytes
+  blockDescription: {
+    countOfBlocksWithThis: number;
+    contentBlockDescription: Array<BlockDescriptor>;
+  };
+};
