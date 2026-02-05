9716534aa37b9e4004fdc9d394d9529f64b1a8a9
Faragó Tamás <ocsi0520@gmail.com>
2026-02-05 17:00:38 +0100

    SNAPSHOT: add simple file input

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import "./style.css";
+import { OtdrConverter } from './otdr/otdr-converter';
+
+const convertButton =
+  document.querySelector<HTMLButtonElement>("button#convert")!;
+const fileInput = document.querySelector<HTMLInputElement>("input[type='file']")!;
+convertButton.onclick = async () => {
+  if (fileInput.files?.length !== 1) {
+    alert('no file was selected');
+    return;
+  }
+  const [file] = fileInput.files;
+  try {
+    // TODO: handle response
+    new OtdrConverter().convert(file);
+
+  } catch (e) {
+    const typedError = e as Error;
+    alert("an error has occurred: " + typedError.message);
+  }
+};
