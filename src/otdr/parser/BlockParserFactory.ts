925b9bd5b018c120257e9ddb469080e2aa8b66eb
Faragó Tamás <ocsi0520@gmail.com>
2026-02-06 16:01:36 +0100

    add rudimentary BlockParserFactory

---
+++ src/otdr/parser/BlockParserFactory.ts
@@ -0,0 +1,27 @@
+import type { OtdrReader } from "../reader/OtdrReader";
+import type { KnownBlockName } from "../representation/MapBlock";
+import { MapBlockParser } from "./MapBlockParser";
+import type { BlockParser } from "./BlockParser";
+
+export class BlockParserFactory {
+  private reader: OtdrReader;
+  constructor(reader: OtdrReader) {
+    this.reader = reader;
+  }
+  public createParserFor(blockName: KnownBlockName | "Map"): BlockParser {
+    switch (blockName) {
+      case "Map":
+        return new MapBlockParser(this.reader);
+      // TODO: implement
+      case "GenParams":
+      case "SupParams":
+      case "FxdParams":
+      case "KeyEvents":
+      case "DataPts":
+      case "LnkParams":
+      case "Cksum":
+      default:
+        throw new Error("no parser found for blockname: " + blockName);
+    }
+  }
+}
