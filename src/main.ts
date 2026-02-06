a7806723dd2cbda33fbc619097c1a4a2f59eb40e
Faragó Tamás <ocsi0520@gmail.com>
2026-02-06 17:30:49 +0100

    hook in all changes into main

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import "./style.css";
-import { OtdrConverter } from './otdr/otdr-converter';
+import { OtdrConverter } from "./otdr/otdr-converter";
 import { OdtrParser } from "./otdr/otdr-parser";
+import { BlockParserFactory } from "./otdr/parser/BlockParserFactory";
+import { ReaderFactory } from "./otdr/reader/ReaderFactory";
 
 const convertButton =
   document.querySelector<HTMLButtonElement>("button#convert")!;
-const fileInput = document.querySelector<HTMLInputElement>("input[type='file']")!;
+const fileInput =
+  document.querySelector<HTMLInputElement>("input[type='file']")!;
 convertButton.onclick = async () => {
   if (fileInput.files?.length !== 1) {
-    alert('no file was selected');
+    alert("no file was selected");
     return;
   }
   const [inputFile] = fileInput.files;
   try {
-    const outputFile = await new OtdrConverter(new OdtrParser()).convert(inputFile);
+    const readerFactory = new ReaderFactory();
+    const reader = await readerFactory.createReader(inputFile);
+    const parserFactory = new BlockParserFactory(reader);
+    const parser = new OdtrParser(parserFactory);
+    const outputFile = await new OtdrConverter(parser).convert(inputFile);
     // TODO: handle response
     console.log(outputFile.name);
   } catch (e) {
