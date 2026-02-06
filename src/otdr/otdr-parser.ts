ecfe03f7e4df22fb8e09608e297afa878675bbb0
Faragó Tamás <ocsi0520@gmail.com>
2026-02-06 17:30:25 +0100

    instantiate mapBlockParser and use it in OdtrParser

--- src/otdr/otdr-parser.ts
+++ src/otdr/otdr-parser.ts
@@ -1,11 +1,20 @@
+import type { BlockParserFactory } from "./parser/BlockParserFactory";
 import type { Representation } from "./representation/Representation";
-
 
 // facade parser, let's have a factory based on the name, which produces a specific parser
 // specific parser parses a part of the representation
 // the first anyway must be map block
 export class OdtrParser {
-    public parse(_arrayBuffer: ArrayBuffer): Representation {
-        throw new Error('not implemented yet');
-    }
+  private blockParserFactory: BlockParserFactory;
+  constructor(parserFactory: BlockParserFactory) {
+    this.blockParserFactory = parserFactory;
+  }
+  public parse(): Representation {
+    const mapBlockParser = this.blockParserFactory.createParserFor("Map");
+
+    let representationSoFar: Partial<Representation> = {};
+    representationSoFar = mapBlockParser.parse(representationSoFar);
+    console.log({ representationSoFar });
+    throw new Error("not implemented yet");
+  }
 }
