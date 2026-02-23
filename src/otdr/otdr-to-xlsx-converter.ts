7b123fcd99065d991f3a9f5744741de923c1c0d1
Faragó Tamás <ocsi0520@gmail.com>
2026-02-23 12:30:58 +0100

    install write-excel-file package

--- src/otdr/otdr-to-xlsx-converter.ts
+++ src/otdr/otdr-to-xlsx-converter.ts
@@ -24,6 +24,9 @@ export class OtdrToXlsxConverter {
   }
 
   private convertRepresentation(_representation: Representation): File {
+    // options: - https://sheetjs.com/ (no styling)
+    //          - https://www.npmjs.com/package/write-excel-file (minimatch - https://gitlab.com/catamphetamine/write-excel-file/-/issues/106)
+    //          - https://www.npmjs.com/package/exceljs (minimatch - https://github.com/exceljs/exceljs/issues/3024)
     throw new Error("xlsx file creation is not implemented yet");
   }
 }
