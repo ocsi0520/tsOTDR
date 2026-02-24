f3bc739129d64f94a9b4d8c0eee4f51a762ec5ef
Faragó Tamás <ocsi0520@gmail.com>
2026-02-24 13:02:51 +0100

    fix type issues caused by package update

--- src/converter/xlsx-converter/XlsxConverter.ts
+++ src/converter/xlsx-converter/XlsxConverter.ts
@@ -1,10 +1,14 @@
 import type { Representation } from "../../otdr/representation/Representation";
-import writeXlsxFile, { type Columns, type SheetData } from "write-excel-file";
+import writeXlsxFile, { type SheetData } from "write-excel-file/browser";
 import type { HeaderCellDataFactory as HeaderCellDataFactory } from "./HeaderCellDataFactory";
 
 // options: - https://sheetjs.com/ (no styling)
-//          - https://www.npmjs.com/package/write-excel-file (minimatch - https://gitlab.com/catamphetamine/write-excel-file/-/issues/106)
+//          - https://www.npmjs.com/package/write-excel-file (fixed minimatch - https://gitlab.com/catamphetamine/write-excel-file/-/issues/106)
 //          - https://www.npmjs.com/package/exceljs (minimatch - https://github.com/exceljs/exceljs/issues/3024)
+
+type Columns = NonNullable<
+  Parameters<typeof writeXlsxFile>[1]["columns"]
+>[number];
 
 export class XlsxConverter {
   private headerCellDataFactory: HeaderCellDataFactory;
