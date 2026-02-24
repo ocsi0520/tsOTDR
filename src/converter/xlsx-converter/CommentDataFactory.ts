4092ce11de587cb13a65b10b343ec3cfecb02520
Faragó Tamás <ocsi0520@gmail.com>
2026-02-24 20:46:50 +0100

    add CommentDataFactory

---
+++ src/converter/xlsx-converter/CommentDataFactory.ts
@@ -0,0 +1,32 @@
+import type { FrameCreator } from "./FrameCreator";
+import type { CellFactory } from "./CellFactory";
+import type { Row, SheetData } from "./excel-types";
+
+export class CommentDataFactory {
+  private cellFactory: CellFactory;
+  private frameCreator: FrameCreator;
+  constructor(cellFactory: CellFactory, frameCreator: FrameCreator) {
+    this.cellFactory = cellFactory;
+    this.frameCreator = frameCreator;
+  }
+
+  public getRows(): SheetData {
+    return this.frameCreator.createFrameFor([
+      this.getCommentRow(),
+      this.cellFactory.getEmptyCells(19),
+      this.cellFactory.getEmptyCells(19),
+      this.cellFactory.getEmptyCells(19),
+    ]);
+  }
+  private getCommentRow(): Row {
+    return this.cellFactory.createSpanCell({
+      value: "Megjegyzés:",
+      rowSpan: 4,
+      span: 19,
+      alignVertical: "top",
+      // TODO: this should be set inside frameCreator, but currently the last row is full of `null`s
+      // it should check for each cell whether its in the last row of the frame
+      bottomBorderStyle: "thick",
+    });
+  }
+}
