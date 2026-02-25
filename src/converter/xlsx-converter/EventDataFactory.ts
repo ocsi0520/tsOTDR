cfe505cfcb7d7b61ffb3da162a2a9400928693a5
Faragó Tamás <ocsi0520@gmail.com>
2026-02-25 12:36:18 +0100

    SNAPSHOT: add event rows

--- src/converter/xlsx-converter/EventDataFactory.ts
+++ src/converter/xlsx-converter/EventDataFactory.ts
@@ -1,3 +1,7 @@
+import type {
+  KeyEvent,
+  KeyEventsSummary,
+} from "../../otdr/representation/KeyEvents";
 import type { Representation } from "../../otdr/representation/Representation";
 import type { CellFactory } from "./CellFactory";
 import type { FrameCreator } from "./FrameCreator";
@@ -6,6 +10,7 @@ import type { Row, SheetData } from "./excel-types";
 const KM_TO_M = 1_000;
 
 // TODO: proper styling
+// TODO: precision to 3
 export class EventDataFactory {
   private cellFactory: CellFactory;
   private frameCreator: FrameCreator;
@@ -89,10 +94,38 @@ export class EventDataFactory {
   }
 
   private getEventRows({ keyEventBlock }: Representation): Row[] {
-    // TODO: finish
-    keyEventBlock.events.slice(0, -1);
+    const { summary } = keyEventBlock;
+    return keyEventBlock.events.map((event) =>
+      this.mapEvenToRow(event, summary),
+    );
+  }
 
-    return [];
+  private mapEvenToRow(keyEvent: KeyEvent, summary: KeyEventsSummary): Row {
+    const distanceSoFar =
+      keyEvent.beginning?.inKm ?? keyEvent.timeOfTravel.calculatedDistance;
+    const distanceToTheEnd = summary.fiberLength.inKm - distanceSoFar;
+
+    const loss = keyEvent.spliceLoss.inDecibel;
+    const reflection = keyEvent.reflectionLoss.inDecibel;
+    const average = (loss + reflection) / 2;
+
+    return [
+      { value: keyEvent.eventNumber },
+      // event->A length // distance so far
+      ...this.cellFactory.createSpanCell({
+        value: distanceSoFar * KM_TO_M,
+        span: 3,
+      }),
+      // event->B length // distance to the end
+      ...this.cellFactory.createSpanCell({
+        value: distanceToTheEnd * KM_TO_M,
+        span: 3,
+        rightBorderStyle: "thick",
+      }),
+      ...this.cellFactory.createSpanCell({ value: loss, span: 2 }),
+      ...this.cellFactory.createSpanCell({ value: reflection, span: 2 }),
+      ...this.cellFactory.createSpanCell({ value: average, span: 2 }),
+    ];
   }
 
   private getBRow({ keyEventBlock }: Representation): Row {
@@ -101,19 +134,28 @@ export class EventDataFactory {
       lastKeyEvent.end?.inKm ?? lastKeyEvent.timeOfTravel.calculatedDistance;
     return [
       { value: "B" },
-      ...this.cellFactory.createSpanCell({ value: 0, span: 3 }),
       ...this.cellFactory.createSpanCell({
         value: endOfLastEvent * KM_TO_M,
         rightBorderStyle: "thick",
         span: 3,
       }),
+      ...this.cellFactory.createSpanCell({ value: 0, span: 3 }),
       ...this.cellFactory.createSpanCell({ value: "", span: 2 }),
       ...this.cellFactory.createSpanCell({ value: "", span: 2 }),
       ...this.cellFactory.createSpanCell({ value: "", span: 2 }),
     ];
   }
 
-  private getFooterRow(_representation: Representation): Row {
+  private getFooterRow({ keyEventBlock }: Representation): Row {
+    const allEvents = keyEventBlock.events;
+    const allLoss = allEvents.reduce(
+      (allLossSoFar, { spliceLoss, reflectionLoss }) =>
+        allLossSoFar + spliceLoss.inDecibel - reflectionLoss.inDecibel,
+      0,
+    );
+    // TODO: fix averageLoss
+    const averageLoss = allLoss / (allEvents.length * 2);
+
     return this.frameCreator.thickenBorder(
       [
         { value: "FILE az.sz.:" },
@@ -133,8 +175,7 @@ export class EventDataFactory {
         }),
         ...this.cellFactory.createSpanCell({
           span: 2,
-          // TODO:
-          value: "???",
+          value: averageLoss,
         }),
       ],
       "topBorderStyle",
